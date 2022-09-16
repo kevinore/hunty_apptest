39a623b099e5cfa31ac5ba343ab226a348a890d2
Kevin <kevinaldana51@gmail.com>
2022-09-16 00:58:13 -0500

    User endpoints and logic added in this feature

---
+++ app/api/v1/core/views.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from app.core.responses import response_format
+
+router = APIRouter()
+
+
+@router.get(
+    "/",
+    description="/",
+    tags=["/"],
+)
+def root():
+    return response_format(
+        {
+            'success': True,
+            'message': 'Hunty test app',
+            'data': "Hey root"
+        }
+    )
+
+
+
+
